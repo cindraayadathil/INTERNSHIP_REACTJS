c696a648e0cd173c7a69e3e04e3024dd64a82121
cindraayadathil <cindraayadathil@gmail.com>
2024-04-22 14:06:25 +0530

    added models for delivery and made apiview using class

--- deliveryapp/urls.py
+++ deliveryapp/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from deliveryapp.views import *
 
+
 urlpatterns = [
-    path('delivery_list', delivery,name='delivery_list')
-
-
+    path('delivery/list', delivery_list,name='delivery_list'),
+    path('delivery/add',delivery_add,name='delivery_add'),
+    path('order/list', OrderList.as_view(), name='order-list'),
+    path('order/add/', OrderAdd.as_view(), name='order-add'),
+    path('order/<int:order_id>/delete/', OrderDelete.as_view(), name='order-delete'),
+    path('order/<int:order_id>/edit/', OrderEdit.as_view(), name='order-edit'),
 ]
--- deliveryapp/views.py
+++ deliveryapp/views.py
@@ -2,13 +2,85 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,APIView
 from rest_framework.response import Response
+from.models import *
+from .serializers import *
+from rest_framework import status
 
-@api_view()
-def delivery(request):
-    return Response({"message":"this is delivery list"})
+@api_view(['GET'])
+def delivery_list(request):
+    if request.method == 'GET':
+        delivery = Delivery.objects.all()
+        serializer = DeliverySerializer(delivery,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
 
-@api_view()
-def delivery2(request):
-    return Response({"message":"this is delivery list"})
+
+@api_view(['GET','POST'])
+def delivery_add(request):
+    if request.method == 'GET':
+        delivery = Delivery.objects.all()
+        serializer = DeliverySerializer(delivery,many=True,context={'request': request})
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = DeliverySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+#  get    
+class OrderList(APIView):
+    def get(self, request, format=None):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+# post 
+class OrderAdd(APIView):
+    def get(self, request, format=None):
+        orders = Order.objects.all() 
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# delete
+class OrderDelete(APIView):
+    def get(self, request, order_id, format=None):
+        order = Order.objects.get(id=order_id)
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, order_id, format=None):
+        order = Order.objects.get(id=order_id)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# patch opertion
+class OrderEdit(APIView):
+    def get(self, request, order_id, format=None):
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, order_id, format=None):
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = OrderSerializer(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
